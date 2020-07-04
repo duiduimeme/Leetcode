5b004b2d6373c6e29a5461d5f821f8f2d96c5c26
duiduimeme <jlulizejin@163.com>
2020-07-04 16:38:45 +0800

    leetcode47 dfs+剪枝

---
+++ src/leetcode63.java
@@ -0,0 +1,15 @@
+public class leetcode63 {
+    /*面试题63、 股票的最大利润
+    *csdn博客地址：https://blog.csdn.net/Lizejin961019/article/details/107126292
+    * */
+    public int maxProfit(int[] prices) {
+        int cost = Integer.MAX_VALUE, res = 0;
+        for(int price : prices) {
+            cost = Math.min(cost, price);
+            res = Math.max(res, price - cost);
+        }
+        return res;
+    }
+
+
+}
